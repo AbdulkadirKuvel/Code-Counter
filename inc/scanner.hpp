10be550e3d59fc7b834ab624ba436ef1086fe64e
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-03-23 23:57:38 +0300

    organized includes and added namespace

--- inc/scanner.hpp
+++ inc/scanner.hpp
@@ -1,18 +1,17 @@
+#pragma once
 #ifndef SCANNER_HPP
 #define SCANNER_HPP
 
 #include <filesystem>
-#include <iostream>
-#include <string>
 #include <vector>
+#include <types.hpp>
 
 namespace fs = std::filesystem;
 
 bool in_whitelist(fs::path);
 bool in_blacklist(fs::path);
-std::vector<fs::path> list_dirs(fs::path);
-std::vector<fs::path> list_dirs_recursive(fs::path);
-std::vector<fs::path> list_files(fs::path);
-std::vector<fs::path> list_files_recursive(fs::path);
+namespace scanner
+{
+}
 
 #endif

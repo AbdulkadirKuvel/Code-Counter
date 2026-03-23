3e844cbadc8c70f61a4860c52eeb72fd4f3b479f
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-03-23 23:59:32 +0300

    organized includes and added namespace

--- inc/lexer.hpp
+++ inc/lexer.hpp
@@ -1,32 +1,23 @@
+#pragma once
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
-#include <string>
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <types.hpp>
 
-struct FileStats
+namespace lexer
 {
-    int file_count = 0;
-    
-    size_t code_line = 0;
-    size_t comment_line = 0;
-    size_t blank_line = 0;
-    size_t total_line = 0;
-};
+    enum DFA_Machine
+    {
+        State0, // start
+        State1, // one character string (')
+        State2, // two characters string (")
+        State3, // in multiline comment (/*) or (""")
+    };
 
-enum DFA_Machine
-{
-    State0, // start
-    State1, // one character string (')
-    State2, // two characters string (")
-    State3, // in multiline comment (/*) or (""")
-};
+    namespace fs = std::filesystem;
 
-namespace fs = std::filesystem;
-
-FileStats file_analyzer_c(fs::path);
-FileStats file_analyzer_py(fs::path);
-
+    types::FileStats file_analyzer_c(fs::path);
+    types::FileStats file_analyzer_py(fs::path);
+}
 #endif

836a8a2645da269290e9269f66d5486979a20a4a
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-03-24 00:01:53 +0300

    organized includes, added namespace and changed vector to unordered set

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,108 +1,74 @@
 #include <scanner.hpp>
+#include <string>
+#include <unordered_set>
 
-std::vector<std::string> blacklist = {
+std::unordered_set<std::string> blacklist = {
     ".git", "bin", "obj", "lib", "node_modules", ".vs"};
 
-std::vector<std::string> whitelist = {
+std::unordered_set<std::string> whitelist = {
     ".py", ".java", ".c", ".cpp", ".h", ".hpp", ".cs",
     ".html", ".css", ".js", ".rb", ".go", ".asm", ".v",
     "makefile"};
 
 bool in_whitelist(fs::path path)
 {
-    for (size_t i = 0; i < whitelist.size(); i++)
-    {
-        if (whitelist.at(i) == path.extension())
-        {
-            return true;
-        }
-    }
+    if (whitelist.find(path.extension().string()) != whitelist.end())
+        return true;
+
     return false;
 }
 
 bool in_blacklist(fs::path path)
 {
-    for (size_t i = 0; i < blacklist.size(); i++)
-    {
-        if (blacklist.at(i) == path.filename())
-        {
-            return true;
-        }
-    }
+    if (blacklist.find(path.extension().string()) != blacklist.end())
+        return true;
     return false;
 }
 
-std::vector<fs::path> list_dirs(fs::path path)
+namespace scanner
 {
-    std::vector<fs::path> paths;
-
-    for (const auto &entry : fs::directory_iterator(path))
+    void scan(types::Config config)
     {
-        if (entry.is_directory())
-        {
-            paths.push_back(entry.path().generic_string());
-        }
-    }
-    return paths;
-}
-
-std::vector<fs::path> list_dirs_recursive(fs::path path)
-{
-    std::vector<fs::path> paths;
-
-    auto it = fs::recursive_directory_iterator(path);
-    auto endit = fs::end(it);
-
-    while (it != endit)
-    {
-        if (it->is_directory())
-        {
-            if (in_blacklist(it->path()))
-                it.disable_recursion_pending();
-            else
-                paths.push_back(it->path().generic_string());
-        }
-        ++it;
-    }
-    return paths;
-}
-
-std::vector<fs::path> list_files(fs::path path)
-{
-    std::vector<fs::path> paths;
-
-    for (const auto &entry : fs::directory_iterator(path))
-    {
-        if (entry.is_regular_file())
-        {
-            paths.push_back(entry.path().generic_string());
-        }
+        return;
     }
 
-    return paths;
-}
-
-std::vector<fs::path> list_files_recursive(fs::path path)
-{
-    std::vector<fs::path> paths;
-
-    auto it = fs::recursive_directory_iterator(path);
-    auto endit = fs::end(it);
-
-    while (it != endit)
+    std::vector<fs::path> list_files(fs::path path)
     {
-        if (it->is_directory())
+        std::vector<fs::path> paths;
+
+        for (const auto &entry : fs::directory_iterator(path))
         {
-            if (in_blacklist(it->path()))
-                it.disable_recursion_pending();
+            if (entry.is_regular_file())
+            {
+                paths.push_back(entry.path().generic_string());
+            }
         }
-        else if (it->is_regular_file())
-        { // when it is a file
-            if (in_whitelist(it->path()))
-                paths.push_back(it->path().generic_string());
-        }
-        ++it;
+
+        return paths;
     }
 
-    return paths;
+    std::vector<fs::path> list_files_recursive(fs::path path)
+    {
+        std::vector<fs::path> paths;
+
+        auto it = fs::recursive_directory_iterator(path);
+        auto endit = fs::end(it);
+
+        while (it != endit)
+        {
+            if (it->is_directory())
+            {
+                if (in_blacklist(it->path()))
+                    it.disable_recursion_pending();
+            }
+            else if (it->is_regular_file())
+            { // when it is a file
+                if (in_whitelist(it->path()))
+                    paths.push_back(it->path().generic_string());
+            }
+            ++it;
+        }
+
+        return paths;
+    }
 }

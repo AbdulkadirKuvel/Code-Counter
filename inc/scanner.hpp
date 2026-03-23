06c4931cacbe98f15f428619cb0bd85f4e0a5b2b
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-03-23 23:58:02 +0300

    removed unused functions

--- inc/scanner.hpp
+++ inc/scanner.hpp
@@ -12,6 +12,9 @@ bool in_whitelist(fs::path);
 bool in_blacklist(fs::path);
 namespace scanner
 {
+    void scan(types::Config);
+    std::vector<fs::path> list_files(fs::path);
+    std::vector<fs::path> list_files_recursive(fs::path);
 }
 
 #endif

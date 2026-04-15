896e15f39003986c8fd4726afbd6f8f39ff9ea84
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-04-15 10:36:49 +0300

    removed specific functions to match "single purpose rule"

--- inc/formatter.hpp
+++ inc/formatter.hpp
@@ -1,13 +1,18 @@
+#pragma once
 #ifndef FORMATTER_HPP
 #define FORMATTER_HPP
 
-#include <iostream>
 #include <filesystem>
+#include <types.hpp>
+#include <map>
 
 namespace fs = std::filesystem;
 
-void print_file_stats(fs::path);
-void print_files_stats(fs::path);
-void print_rec_files_stats(fs::path);
+namespace formatter
+{
+    void print_files_stats(std::map<std::string, types::FileStats>);
+    void log_files_stats(std::map<std::string, types::FileStats>, std::string);
+    void print_help();
+}
 
 #endif

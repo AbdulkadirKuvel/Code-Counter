a7a782ddb66bbed0786d9cce4ed4808454b627f7
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-06-20 07:21:26 +0300

    improved the command parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,76 +1,111 @@
 #include <parser.hpp>
+#include <print>
 
 types::Config parser(int argc, char *argv[])
 {
     fs::path path;
     types::Config config;
+
     if (argc >= 2)
     {
         // bool in_flag = 0;
 
-        bool config_file = true;
-        std::string config_path;
         // First iterator: defines if user wants to get help, or use a prepared config file
+        bool config_use = false;
+        bool config_file_name_exists = false;
         for (int i = 1; i < argc; i++)
         {
             std::string_view arg = argv[i];
+            // print the help and shut down the app
             if (arg == "-h" || arg == "--help")
             {
-                formatter::print_help();
                 config.help_requested = true;
                 return config;
             }
-
-            if (config_file)
+            else if (arg == "-c" || arg == "--config") // CONFIG USE HAS BEEN DETECTED
             {
-                if (arg[0] != '-')
-                { // CONFIG FILE NAME HAS BEEN DETECTED
-                    config_path = arg;
-                }
-                else
-                { // CONFIG FILE NAME HAS BEEN ASSIGNED TO DEFAULT
-                    config_path = "config.json";
-                }
-            }
-            else
-            {
-
-                if (arg == "-c" || arg == "--config")
+                config_use = true;
+                if (i + 1 < argc)
                 {
-                    config_file = true;
+                    if (argv[i + 1][0] != '-') // CONFIG FILE NAME HAS BEEN CONFIGURED
+                    {
+                        fs::path target(argv[i + 1]);
+
+                        if (fs::is_regular_file(target)) // Given path is a filename
+                        {
+                            config.config_path = argv[i + 1];
+                            config_file_name_exists = true;
+                            i++;
+                        }
+                        else if (fs::is_directory(target)) // Given path is not a folder: Error
+                        {
+                            // This is a path, error
+                            config.error_requested = true;
+                            config.error_info.title = "Not a File Error";
+                            config.error_info.message = "Given path \"", target, "\" is a directory.";
+                            return config;
+                        }
+                        else // Given argument neither a file or folder: Error
+                        {
+                            config.error_requested = true;
+                            config.error_info.title = "False File Path Error";
+                            config.error_info.message = "Given argument \"", argv[i + 1], "\" is not a config file.";
+                            return config;
+                        }
+                    }
+                    else // Config filename has been forgotten: Error
+                    {
+                        config.error_requested = true;
+                        config.error_info.title = "Missing Filename Error";
+                        config.error_info.message = "Config flag has been risen but no file indicated.";
+                        return config;
+                    }
+                }
+                else {
+                        config.error_requested = true;
+                        config.error_info.title = "Missing Filename Error";
+                        config.error_info.message = "Config flag has been risen but no file indicated.";
+                        return config;
+
                 }
             }
         }
 
-        while (config_file)
+        // Reading the config file
+        if (config_use)
         {
-            std::ifstream file(config_path, std::ios::in);
+            std::ifstream file(config.config_path, std::ios::in);
             if (!file.is_open())
             {
-                std::println("File \"{}\" could not open for reading", config_path);
-                break;
+                config.error_requested = true;
+                config.error_info.title = "File Could Not Open";
+                config.error_info.message = "File \"" + config.config_path.generic_string() + "\" could not open for reading.";
+                return config;
             }
-
-            std::string line;
-            while (std::getline(file, line))
+            else
             {
-                for (char c : line)
+                std::string line;
+                // Second iterator: reads the config file.
+                while (std::getline(file, line))
                 {
-                    if (isspace(c))
+                    if (line == "")
                         continue;
-                    else if (c == '#') // this is a comment
+                    else if (line[0] == '#') // comment
                     {
-                        break;
                     }
-                    else // this is not a comment
+                    else // must be code
                     {
-                        int index = line.find('=');
-                        if (index)
-                        { // key=value has been found
-                            std::string_view key = line.substr(0, index);
-                            std::string_view value = line.substr(index + 1);
-
-                            bool val = tolower(value[0]) == 't';
+                        // e.g: "recursive = true"
+                        size_t index = line.find('=');
+                        if (index != std::string::npos)
+                        {
+                            std::string_view key = line.substr(0, index); // e.g: "recursive"
+                            std::string_view value = line.substr(index + 1); // e.g: "true"
+                            if (value.empty())
+                            {
+                                continue; // ADD FEATURE: Throw error
+                            }
+                            bool val = tolower(value[0]) == 't'; // "true" or "false"
 
                             if (key == "recursive")
                             {
@@ -84,34 +119,124 @@ types::Config parser(int argc, char *argv[])
                             {
                                 config.output_json = val;
                             }
-                            else if (key == "blacklist") {
-                                // config.blacklist = value; // TODO
+                            else if (key == "blacklist")
+                            {
+                                config.blacklist.clear();
+
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.blacklist.insert(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                config.blacklist.insert(std::string(value.substr(start)));
                             }
-                            else if (key == "whitelist") {
-                                // config.whitelist = value // TODO
+                            else if (key == "whitelist")
+                            {
+                                config.whitelist.clear();
+
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.whitelist.insert(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                config.whitelist.insert(std::string(value.substr(start)));
+                            }
+                            else if (key == "add_blacklist")
+                            {
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.blacklist.insert(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                config.blacklist.insert(std::string(value.substr(start)));
+                            }
+                            else if (key == "add_whitelist")
+                            {
+
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.whitelist.insert(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                config.whitelist.insert(std::string(value.substr(start)));
+                            }
+                            else if (key == "remove_blacklist")
+                            {
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.blacklist.erase(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                config.blacklist.erase(std::string(value.substr(start)));
+                            }
+                            else if (key == "remove_whitelist")
+                            {
+                                size_t start = 0;
+                                size_t end = value.find(',');
+
+                                while (end != std::string::npos)
+                                {
+                                    std::string item = std::string(value.substr(start, end - start));
+                                    config.whitelist.erase(item);
+
+                                    start = end + 1;
+                                    end = value.find(',', start);
+                                }
+                                // Do not miss the last word
+                                config.whitelist.erase(std::string(value.substr(start)));
+                            }
+                            else
+                            {
+                                // none
                             }
                         }
-                        else // Not understandable
+                        else
                         {
+                            // pass it, whatever it is.
                         }
                     }
                 }
             }
-
             file.close();
         }
 
+        // Third iterator: configuration from console (most authorization)
         for (int i = 1; i < argc; i++)
         {
             std::string_view arg = argv[i];
 
-            if (arg == "-h" || arg == "--help")
-            {
-                formatter::print_help();
-                config.help_requested = true;
-                return config;
-            }
-            else if (arg == "-r" || arg == "--recursive")
+            // "-h" or "--help" will be fore-handled in first iterator
+            if (arg == "-r" || arg == "--recursive")
             {
                 config.recursive = true;
             }
@@ -122,6 +247,13 @@ types::Config parser(int argc, char *argv[])
             else if (arg == "-j" || arg == "--json")
             {
                 config.output_json = true;
+            }
+            else if (arg == "-c" || arg == "--config")
+            {
+                if (!config_file_name_exists)
+                    continue;
+                else
+                    i++;
             }
             else
             {

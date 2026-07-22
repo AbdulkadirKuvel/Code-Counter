03604507745caa814f8c69376bd66eed7968ca0a
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-07-23 01:01:35 +0300

    created an extra util function

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -2,17 +2,25 @@
 
 namespace utils
 {
-    // Başındaki ve sonundaki boşlukları, satır sonlarını ve tırnakları temizler
     std::string_view trim(std::string_view str)
     {
-        // Silinmesini istediğimiz görünmez karakterler ve tırnak işareti
         const char *whitespace_and_quotes = " \t\r\n\"";
 
         size_t first = str.find_first_not_of(whitespace_and_quotes);
         if (first == std::string_view::npos)
-            return ""; // Sadece boşluktan oluşuyorsa
+            return "";
 
         size_t last = str.find_last_not_of(whitespace_and_quotes);
         return str.substr(first, (last - first + 1));
+    }
+
+    constexpr std::string_view unquote_and_trim(std::string_view sv) noexcept {
+        sv = trim(sv);
+        if (sv.size() > 2 && ((sv.front() == '"' && sv.back() == '"') || (sv.front() == '\'' && sv.back() == '\'')))
+        {
+            sv.remove_prefix(1);
+            sv.remove_suffix(1);
+        }
+        return trim(sv);
     }
 }

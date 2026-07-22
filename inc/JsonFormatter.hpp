b0c6113e5b7201f24498d5b5671a159b1b02a05a
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-07-23 00:09:28 +0300

    added json formatting style

--- inc/JsonFormatter.hpp
+++ inc/JsonFormatter.hpp
@@ -1,7 +1,34 @@
 // TODO: Feature
 #include <IReportFormatter.hpp>
+#include <ostream>
 #include <types.hpp>
 
-class JsonFormatter : public IReportFormatter {
-    
+class JsonFormatter : public IReportFormatter
+{
+public:
+    void format(std::ostream &os, const std::map<std::string, types::FileStats> &stats) const override
+    {
+        std::println(os, "{{");
+        std::println(os, "  \"files\": [");
+
+        size_t index = 0;
+        const size_t total_items = stats.size();
+
+        for (const auto& [ext, stat] : stats)
+        {
+            const bool is_last = (++index == total_items);
+
+            std::println(os, "    {{");
+            std::println(os, "    \"extension\": \"{}\",",ext);
+            std::println(os, "    \"file_count\": \"{}\",",stat.file_count);
+            std::println(os, "    \"total_lines\": \"{}\",",stat.total_line);
+            std::println(os, "    \"code_lines\": \"{}\",",stat.code_line);
+            std::println(os, "    \"comment_lines\": \"{}\",",stat.comment_line);
+            std::println(os, "    \"blank_lines\": \"{}\"",stat.blank_line);
+            std::println(os, "    }}{}", is_last ? "" : ",");
+        }
+        
+        std::println(os, "  ]");
+        std::println(os, "}}");
+    }
 };
